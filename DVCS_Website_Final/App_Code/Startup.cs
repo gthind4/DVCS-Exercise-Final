ef4c560253b54db497adb7dd03505d2f9b8b34f4
Gurvinderpal Singh Thind <gthind4@nait.ca>
2015-09-18 19:11:09 -0600

    Initial Project and Solution Setup

---
+++ DVCS_Website_Final/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DVCS_Website_Final.Startup))]
+namespace DVCS_Website_Final
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
